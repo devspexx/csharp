d8a52f53c65a19260ab824d036250e356e7ced9b
Sp3eex <46614224+Sp3eex@users.noreply.github.com>
2019-09-15 10:53:34 +0200

    Add files via upload

---
+++ GoogleTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickFix.Classes
+{
+    class GoogleTime
+    {
+        public async static Task<DateTime> GetCurrentTime() {
+            using (var client = new HttpClient()) {
+                try
+                {
+                    var result = await client.GetAsync("https://google.com",
+                          HttpCompletionOption.ResponseHeadersRead);
+                    return result.Headers.Date.Value.DateTime.AddHours(2); // dodaj 2 uri zaostanka
+                }
+                catch {
+                    // ce ni interneta zacasno returnaj lokalni cajt
+                    return DateTime.Now;
+                }
+            }
+        }
+    }
+}
---
+++ TextUtils.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickFix.Classes
+{
+    public class TextUtils
+    {
+        public static bool TextContainsOnlyLatters(string text) {
+            if (text.All(Char.IsLetter)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool TextContainsNumbersLatters(string text) {
+            if (text.All(Char.IsLetterOrDigit)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool TextContainsOnlyNumbers(string text) {
+            if (text.All(Char.IsDigit)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool TextContainsSymbols(string text) {
+            if (text.All(Char.IsSymbol)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool TextContainsSpaces(string text) {
+            if (text.All(Char.IsWhiteSpace)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool ListContainsString(List<string> list, string text) {
+            if (list.Where(CheckString => CheckString.Contains(text)) != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static List<string> ListAddStringsToList(List<string> list, string[] strings) {
+            for (int i = 0; i < strings.Length; i++) {
+                list.Add(strings[i]);
+            }
+            return list;
+        }
+
+        public static List<string> ListRemoveStringsFromList(List<string> list, string[] strings) {
+            for (int i = 0; i < strings.Length; i++) {
+                list.Remove(strings[i]);
+            }
+            return list;
+        }
+
+        private static Random random = new Random();
+        public static string RandomChars(int dolzina) {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, dolzina)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomNums(int dolzina) {
+            const string chars = "1234567890";
+            return new string(Enumerable.Repeat(chars, dolzina)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
