2d803992599d9a547d78a98720eefd160c0b287e
Sp3eex <janez1vodusek@gmail.com>
2019-10-22 21:23:40 +0200

    Bordless Form animations
    c# .net winforms 4.7.2

---
+++ BordlessForm/Skribl/UI/Forms/FormBase.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuickFix.UI.Forms
+{
+    public class FormBase : Form
+    {
+
+        public void DecorationMouseDown(HitTestValues hit, Point p)
+        {
+            NativeMethods.ReleaseCapture();
+            var pt = new POINTS { X = (short)p.X, Y = (short)p.Y };
+            NativeMethods.SendMessage(Handle, (int)WindowMessages.WM_NCLBUTTONDOWN, (int)hit, pt);
+        }
+
+        public void DecorationMouseDown(HitTestValues hit)
+        {
+            DecorationMouseDown(hit, MousePosition);
+        }
+
+        public void DecorationMouseUp(HitTestValues hit, Point p)
+        {
+            NativeMethods.ReleaseCapture();
+            var pt = new POINTS { X = (short)p.X, Y = (short)p.Y };
+            NativeMethods.SendMessage(Handle, (int)WindowMessages.WM_NCLBUTTONUP, (int)hit, pt);
+        }
+
+        public void DecorationMouseUp(HitTestValues hit)
+        {
+            DecorationMouseUp(hit, MousePosition);
+        }
+        
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            if (!DesignMode)
+                SetWindowRegion(Handle, 0, 0, Width, Height);
+        }
+
+        protected void ShowSystemMenu(MouseButtons buttons)
+        {
+            ShowSystemMenu(buttons, MousePosition);
+        }
+
+        protected static int MakeLong(short lowPart, short highPart)
+        {
+            return (int)(((ushort)lowPart) | (uint)(highPart << 16));
+        }
+
+        protected void ShowSystemMenu(MouseButtons buttons, Point pos)
+        {
+            NativeMethods.SendMessage(Handle, (int)WindowMessages.WM_SYSMENU, 0, MakeLong((short)pos.X, (short)pos.Y));
+        }
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect, // x-coordinate of upper-left corner
+            int nTopRect, // y-coordinate of upper-left corner
+            int nRightRect, // x-coordinate of lower-right corner
+            int nBottomRect, // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        [DllImport("dwmapi.dll")] public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+
+        [DllImport("dwmapi.dll")] public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        [DllImport("dwmapi.dll")] public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+
+        private bool m_aeroEnabled;
+        private const int CS_DROPSHADOW = 0x00020000;
+        private const int WM_NCPAINT = 0x0085;
+        private const int WM_ACTIVATEAPP = 0x001C;
+
+        public struct MARGINS {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        private bool CheckAeroEnabled() {
+            if (Environment.OSVersion.Version.Major >= 6) {
+                int enabled = 0;
+                DwmIsCompositionEnabled(ref enabled);
+                return (enabled == 1) ? true : false;
+            }
+            return false;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (DesignMode)
+            {
+                base.WndProc(ref m);
+                return;
+            }
+
+            switch (m.Msg)
+            {
+                case (int)WindowMessages.WM_NCCALCSIZE:
+                    {
+                        // Provides new coordinates for the window client area.
+                        WmNCCalcSize(ref m);
+                        break;
+                    }
+                case (int)WindowMessages.WM_NCPAINT:
+                    {
+                        //if (m_aeroEnabled)
+                        //{
+                        //    var v = 2;
+                        //    DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+                        //    MARGINS margins = new MARGINS()
+                        //    {
+                        //        bottomHeight = 1,
+                        //        leftWidth = 0,
+                        //        rightWidth = 0,
+                        //        topHeight = 0
+                        //    };
+                        //    DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+                        //}
+                        WmNCPaint(ref m);
+                        break;
+                    }
+                case (int)WindowMessages.WM_NCACTIVATE:
+                    {
+                        // ... WM_NCACTIVATE does some painting directly 
+                        // without bothering with WM_NCPAINT ...
+                        WmNCActivate(ref m);
+                        break;
+                    }
+                case (int)WindowMessages.WM_SETTEXT:
+                    {
+                        // ... and some painting is required in here as well
+                        WmSetText(ref m);
+                        break;
+                    }
+                case (int)WindowMessages.WM_WINDOWPOSCHANGED:
+                    {
+                        WmWindowPosChanged(ref m);
+                        break;
+                    }
+                case 174: // ignore magic message number
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        base.WndProc(ref m);
+                        break;
+                    }
+            }
+        }
+
+        private void SetWindowRegion(IntPtr hwnd, int left, int top, int right, int bottom)
+        {
+            var rgn = NativeMethods.CreateRectRgn(0, 0, 0, 0);
+            var hrg = new HandleRef((object)this, rgn);
+            var r = NativeMethods.GetWindowRgn(hwnd, hrg.Handle);
+            RECT box;
+            NativeMethods.GetRgnBox(hrg.Handle, out box);
+            if (box.left != left || box.top != top || box.right != right || box.bottom != bottom)
+            {
+                var hr = new HandleRef((object)this, NativeMethods.CreateRectRgn(left, top, right, bottom));
+                NativeMethods.SetWindowRgn(hwnd, hr.Handle, NativeMethods.IsWindowVisible(hwnd));
+            }
+            NativeMethods.DeleteObject(rgn);
+        }
+
+        public FormWindowState MinMaxState
+        {
+            get
+            {
+                var s = NativeMethods.GetWindowLong(Handle, NativeConstants.GWL_STYLE);
+                var max = (s & (int)WindowStyle.WS_MAXIMIZE) > 0;
+                if (max) return FormWindowState.Maximized;
+                var min = (s & (int)WindowStyle.WS_MINIMIZE) > 0;
+                if (min) return FormWindowState.Minimized;
+                return FormWindowState.Normal;
+            }
+        }
+
+        private void WmWindowPosChanged(ref Message m)
+        {
+            DefWndProc(ref m);
+            UpdateBounds();
+            var pos = (WINDOWPOS)Marshal.PtrToStructure(m.LParam, typeof(WINDOWPOS));
+            SetWindowRegion(m.HWnd, 0, 0, pos.cx, pos.cy);
+            m.Result = NativeConstants.TRUE;
+        }
+
+        private void WmNCCalcSize(ref Message m)
+        {
+            // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/windowsuserinterface/windowing/windows/windowreference/windowmessages/wm_nccalcsize.asp
+            // http://groups.google.pl/groups?selm=OnRNaGfDEHA.1600%40tk2msftngp13.phx.gbl
+
+            MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+
+            var r = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
+            var max = MinMaxState == FormWindowState.Maximized;
+
+            if (max) {
+                var x = NativeMethods.GetSystemMetrics(NativeConstants.SM_CXSIZEFRAME);
+                var y = NativeMethods.GetSystemMetrics(NativeConstants.SM_CYSIZEFRAME);
+                var p = NativeMethods.GetSystemMetrics(NativeConstants.SM_CXPADDEDBORDER+10);
+                
+                // fix black line on the borders
+                var w = (x + p) - 5;
+                var h = (y + p) - 5;
+
+                r.left += w;
+                r.top += h;
+                r.right -= w;
+                r.bottom -= h;
+
+                var appBarData = new APPBARDATA();
+                appBarData.cbSize = Marshal.SizeOf(typeof(APPBARDATA));
+                var autohide = (NativeMethods.SHAppBarMessage(NativeConstants.ABM_GETSTATE, ref appBarData) & NativeConstants.ABS_AUTOHIDE) != 0;
+                if (autohide) r.bottom -= 1;
+
+                Marshal.StructureToPtr(r, m.LParam, true);
+            }
+            m.Result = IntPtr.Zero;
+        }
+
+        private void WmNCPaint(ref Message msg)
+        {
+            // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/gdi/pantdraw_8gdw.asp
+            // example in q. 2.9 on http://www.syncfusion.com/FAQ/WindowsForms/FAQ_c41c.aspx#q1026q
+
+            // The WParam contains handle to clipRegion or 1 if entire window should be repainted
+            //PaintNonClientArea(msg.HWnd, (IntPtr)msg.WParam);
+
+            // we handled everything
+            msg.Result = NativeConstants.TRUE;
+        }
+
+        private void WmSetText(ref Message msg)
+        {
+            // allow the system to receive the new window title
+            DefWndProc(ref msg);
+
+            // repaint title bar
+            //PaintNonClientArea(msg.HWnd, (IntPtr)1);
+        }
+
+        private void WmNCActivate(ref Message msg)
+        {
+            // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/windowsuserinterface/windowing/windows/windowreference/windowmessages/wm_ncactivate.asp
+
+            bool active = (msg.WParam == NativeConstants.TRUE);
+
+            if (MinMaxState == FormWindowState.Minimized)
+                DefWndProc(ref msg);
+            else
+            {
+                // repaint title bar
+                //PaintNonClientArea(msg.HWnd, (IntPtr)1);
+
+                // allow to deactivate window
+                msg.Result = NativeConstants.TRUE;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormBase
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FormBase";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ BordlessForm/Skribl/UI/Forms/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using QuickFix.UI.Forms;
+using System.Windows.Forms;
+
+namespace BorderlessForm
+{
+    public partial class MainForm : FormBase
+    {
+        private FormWindowState previousWindowState;
+        public MainForm() {
+            InitializeComponent();
+            //MinimizeLabel.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) WindowState = FormWindowState.Minimized; };
+            //MaximizeLabel.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) ToggleMaximize(); };
+            //previousWindowState = MinMaxState;
+            //CloseLabel.MouseClick += (s, e) => Close(e);
+        }
+        
+        void Close(MouseEventArgs e)  {
+            if (e.Button == MouseButtons.Left) Close();
+        }
+
+        private FormWindowState ToggleMaximize() {
+            return WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+        }
+    }
+}
