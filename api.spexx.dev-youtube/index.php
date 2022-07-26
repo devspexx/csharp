e0c977c018a68f0ca96b3069ff7b5dd1b76f5be7
spexx <46614224+devspexx@users.noreply.github.com>
2022-07-26 20:22:40 +0200

    api.spexx.dev/youtube

--- api.spexx.dev-youtube/index.php
+++ api.spexx.dev-youtube/index.php
@@ -9,8 +9,6 @@
     <link rel="preconnect" href="//fonts.gstatic.com" crossorigin="">
     <link rel="dns-prefetch" href="//fonts.gstatic.com" crossorigin="">
     <link href="//fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700" rel="stylesheet">
-    <link rel="shortcut icon" href="https://spexx.dev/assets/images/branding/favicon.ico">
-
     <style>
         body{margin:0 auto;padding:0;font-family:'Readex Pro';display:flex;flex-direction:column;max-width:800px;min-height:100vh;}.searcher{display:flex;flex-direction:column;padding:50px 0 10px 0}.searcher input{height:35px;margin:10;padding:10;font-family:'Readex Pro';font-weight:400;font-size:15px;border:2px solid #F2F2F2;border-radius:15px;outline:none;text-indent:10px} input:focus{border-color: rgb(220, 220, 220)}iframe{width:100%;min-height:1100px}
     </style>
@@ -24,8 +22,6 @@
     </div>
 </body>
 <script defer>
-
 var _0x3e43b5=_0x1dc3;function _0x3ac9(){var _0x1b9948=['3223990rTFuwn','keypress','https://yt-download.org/api/widget?url=','getElementById','120UcSfpD','358711TirnQa','53220TdnWjF','19607687ZEsHsz','src','key','80uHaypE','addEventListener','129372zFMvUP','setAttribute','579906jDHcve','Enter','length','searcher','loader','value','2dtpVrs','30RtcVCB','split','9679978Fotpaj'];_0x3ac9=function(){return _0x1b9948;};return _0x3ac9();}function _0x1dc3(_0x37a80d,_0x5609af){var _0x3ac931=_0x3ac9();return _0x1dc3=function(_0x1dc3ab,_0xb370a4){_0x1dc3ab=_0x1dc3ab-0x162;var _0x488ab3=_0x3ac931[_0x1dc3ab];return _0x488ab3;},_0x1dc3(_0x37a80d,_0x5609af);}(function(_0x573c55,_0x2d7479){var _0x3827a2=_0x1dc3,_0x2828ba=_0x573c55();while(!![]){try{var _0x2b17e0=-parseInt(_0x3827a2(0x173))/0x1*(-parseInt(_0x3827a2(0x16a))/0x2)+-parseInt(_0x3827a2(0x16b))/0x3*(parseInt(_0x3827a2(0x174))/0x4)+-parseInt(_0x3827a2(0x178))/0x5*(-parseInt(_0x3827a2(0x162))/0x6)+parseInt(_0x3827a2(0x16d))/0x7+-parseInt(_0x3827a2(0x172))/0x8*(-parseInt(_0x3827a2(0x164))/0x9)+-parseInt(_0x3827a2(0x16e))/0xa+-parseInt(_0x3827a2(0x175))/0xb;if(_0x2b17e0===_0x2d7479)break;else _0x2828ba['push'](_0x2828ba['shift']());}catch(_0x4eb8fa){_0x2828ba['push'](_0x2828ba['shift']());}}}(_0x3ac9,0xc6ffd),document[_0x3e43b5(0x171)]('searcher')[_0x3e43b5(0x179)](_0x3e43b5(0x16f),function(_0x1431c2){var _0x2d4601=_0x3e43b5;if(_0x1431c2[_0x2d4601(0x177)]===_0x2d4601(0x165)){var _0x56acaa=_0x2d4601(0x170),_0x362b77=document[_0x2d4601(0x171)](_0x2d4601(0x168)),_0x34dac9=document[_0x2d4601(0x171)](_0x2d4601(0x167));_0x362b77[_0x2d4601(0x176)]=_0x56acaa+_0x362b77[_0x2d4601(0x169)];if(_0x34dac9[_0x2d4601(0x169)]==''||_0x34dac9[_0x2d4601(0x169)]=='\x20'||_0x34dac9[_0x2d4601(0x169)][_0x2d4601(0x16c)]('=')[0x1][_0x2d4601(0x166)]<0xb)return;else _0x362b77[_0x2d4601(0x163)]('src',_0x56acaa+_0x34dac9[_0x2d4601(0x169)]);}}));
-
 </script>
 </html>
