956b65cacb6e342dda9b30d54037265a934e2696
spexx <janez1vodusek@gmail.com>
2022-08-05 19:24:15 +0200

    clean-temp-files

---
+++ project-clean-temp-files/cleantempfiles/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace cleantempfiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "clean temp files && empty recycle bin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 56);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 9);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "C:\\Windows\\Temp";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "C:\\Users\\username\\AppData\\Local\\Temp";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "C:/Temp";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 104);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(510, 143);
+            this.MinimumSize = new System.Drawing.Size(510, 143);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ project-clean-temp-files/cleantempfiles/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cleantempfiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum RecycleFlags : uint
+        {
+            SHERB_NOCONFIRMATION = 0x00000001,
+            SHERB_NOPROGRESSUI = 0x00000002,
+            SHERB_NOSOUND = 0x00000004
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+
+        private bool isProgressing = false;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (isProgressing) return;
+            isProgressing = true;
+
+            string path = "C:/Users/" + Environment.UserName + "/AppData/Local/Temp";
+            string path2 = "C:/Windows/Temp";
+            string path3 = "C:/temp";
+
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("%TEMP% does not exist:: " + path);
+                return;
+            }
+
+            List<string> dirs = new List<string>();
+            List<string> files = new List<string>();
+
+            foreach (string dir in Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories))
+            {
+                dirs.Add(dir);
+                label1.Text = "found dir: " + dir;
+                foreach (string file in Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories))
+                {
+                    label1.Text = "found file: " + file;
+                    files.Add(file);
+                }
+                await Task.Delay(1);
+            }
+            foreach (string dir in Directory.GetDirectories(path2, "*.*", SearchOption.AllDirectories))
+            {
+                dirs.Add(dir);
+                label1.Text = "found dir: " + dir;
+                foreach (string file in Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories))
+                {
+                    label1.Text = "found file: " + file;
+                    files.Add(file);
+                }
+                await Task.Delay(1);
+            }
+            if (Directory.Exists("C:/temp"))
+            {
+                foreach (string dir in Directory.GetDirectories(path3, "*.*", SearchOption.AllDirectories))
+                {
+                    dirs.Add(dir);
+                    label1.Text = "found dir: " + dir;
+                    foreach (string file in Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories))
+                    {
+                        label1.Text = "found file: " + file;
+                        files.Add(file);
+                    }
+                    await Task.Delay(1);
+                }
+            }
+
+            await Task.Delay(1000);
+            label1.Text = "starting deletion in 3..";
+            await Task.Delay(1000);
+            label1.Text = "starting deletion in 2..";
+            await Task.Delay(1000);
+            label1.Text = "starting deletion in 1..";
+            await Task.Delay(1000);
+            label1.ForeColor = Color.IndianRed;
+            progressBar1.Maximum = dirs.Count;
+            foreach (string dir in dirs)
+            {
+                progressBar1.Value++;
+                label1.Text = "deleting: " + dir;
+                
+                try
+                {
+                    Directory.Delete(dir, true);
+                } catch (Exception) { }
+                await Task.Delay(1);
+                
+            }
+
+            // arg index 1 = If this value is an empty string or NULL, all Recycle Bins on all drives will be emptied.
+            // arg index 2 = don't show progress reports/confirmation windows
+            SHEmptyRecycleBin(Handle, null, RecycleFlags.SHERB_NOCONFIRMATION);
+
+            label1.Text = "deleted dirs: " + dirs.Count + ", files: " + files.Count;
+
+            Application.Restart();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "C:/Users/" + Environment.UserName + "/AppData/Local/Temp";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", label4.Text);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", label3.Text);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", label2.Text);
+        }
+    }
+}
